6ae70258245073f07b8bddd0088419ba5103e0cc
yadavkundesh2023 <yadavkundesh.tester111@gmail.com>
2024-01-21 01:03:25 +0530

    cgc

--- src/main/java/com/eva/vtiger/utils/GitTest.java
+++ src/main/java/com/eva/vtiger/utils/GitTest.java
@@ -25,4 +25,21 @@ public static void method4() {
 	System.out.println("i am method4");
 	
 }
+//public static void method1() {
+//	System.out.println("i am method1");
+//	
+//}
+//
+//public static void method2() {
+//	System.out.println("i am method2");
+//	
+//}
+//public static void method3() {
+//	System.out.println("i am method3");
+//	
+//}
+//public static void method4() {
+//	System.out.println("i am method4");
+//	
+//}
 }
