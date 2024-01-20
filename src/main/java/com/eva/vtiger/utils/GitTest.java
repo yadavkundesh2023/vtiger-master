0e1b400321d8e8ac0d7880de1aaa7a8a62cf9eff
yadavkundesh2023 <yadavkundesh.tester111@gmail.com>
2024-01-21 00:20:07 +0530

    method$

--- src/main/java/com/eva/vtiger/utils/GitTest.java
+++ src/main/java/com/eva/vtiger/utils/GitTest.java
@@ -4,10 +4,25 @@ public class GitTest {
 public static void main(String[] args) {
 	System.out.println("i am main method");
 	method1();
-	
+	method2();
+	method3();
+
 }
 public static void method1() {
+	System.out.println("i am method1");
+	
+}
+
+public static void method2() {
 	System.out.println("i am method2");
+	
+}
+public static void method3() {
+	System.out.println("i am method3");
+	
+}
+public static void method4() {
+	System.out.println("i am method4");
 	
 }
 }
